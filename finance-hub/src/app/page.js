4f91db1ed94bb22fb2eea0b0d763568ccf74ac3e
DanielFeldman1 <daniel.feldman303@gmail.com>
2025-06-06 17:34:30 +0300

    Added Net Worth on top of the page

--- finance-hub/src/app/page.js
+++ finance-hub/src/app/page.js
@@ -5,25 +5,52 @@ import bankData from "./data/bankData.json";
 import pensionData from "./data/pensionData.json";
 import savingsData from "./data/savingsData.json";
 
+const allData = [bankData, savingsData, pensionData];
+const calcNetWorth = allData.reduce((sum, json) => {
+  const last = json[json.length - 1];
+  return sum + (last.balance || 0);
+}, 0);
+const netWorthThousand =
+  Math.floor((calcNetWorth + investmentsData[investmentsData.length - 1].balance * 3.49) /
+  1000);
+const netWorthHundrads =
+  (calcNetWorth + investmentsData[investmentsData.length - 1].balance * 3.49) %
+  1000;
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="bg-gray-100 min-h-screen">
-        <div>
-          <h1>Net Worth: {}</h1>
+        <div className="bg-white shadow-md rounded-2xl border-1 border-gray-300 m-10 p-10">
+          <h1>{`Net Worth: ${netWorthThousand},${netWorthHundrads} ILS`}</h1>
         </div>
         <div className="grid grid-cols-4">
           <div>
-            <GraphCard title="Investments" chartData={investmentsData} graphColor="#D30000" />
+            <GraphCard
+              title="Investments"
+              chartData={investmentsData}
+              graphColor="#D30000"
+            />
           </div>
           <div>
-            <GraphCard title="Bank Balance" chartData={bankData} graphColor="#00cc00" />
+            <GraphCard
+              title="Bank Balance"
+              chartData={bankData}
+              graphColor="#00cc00"
+            />
           </div>
           <div>
-            <GraphCard title="Savings" chartData={savingsData} graphColor="#0062cc"/>
+            <GraphCard
+              title="Savings"
+              chartData={savingsData}
+              graphColor="#0062cc"
+            />
           </div>
           <div>
-            <GraphCard title="Pension" chartData={pensionData} graphColor="#FFE135" />
+            <GraphCard
+              title="Pension"
+              chartData={pensionData}
+              graphColor="#FFE135"
+            />
           </div>
         </div>
       </div>
